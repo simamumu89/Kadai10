82b17776154e71d4dfb12b2a9c7fa2b83f5935a9
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-27 22:27:16 +0900

    dockerまわりの設定を追加

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS patientcharts;
+
+CREATE TABLE patientcharts (
+ id int unsigned AUTO_INCREMENT,
+ name VARCHAR(20) NOT NULL,
+ gender VARCHAR(20) NOT NULL,
+ address VARCHAR(20) NOT NULL,
+ insurancecard VARCHAR(20) NOT NULL,
+ medicalhistory VARCHAR(20) NOT NULL,
+ PRIMARY KEY(id)
+);
+
+INSERT INTO patientcharts (id,name,gender,address,insurancecard,medicalhistory) VALUES (1, "島村", "男性", "沖縄", "協会", "心臓病");
+INSERT INTO patientcharts (id,name,gender,address,insurancecard,medicalhistory) VALUES (2, "鈴木", "男性", "福岡", "国保", "肺癌");
+INSERT INTO patientcharts (id,name,gender,address,insurancecard,medicalhistory) VALUES (3, "広瀬", "女性", "東京", "健保", "胃癌");
