e37b18101c72871c2ca992f929f9b70e729f4fd1
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-27 20:53:35 +0900

    “プロジェクトをセットアップ”

---
+++ src/main/java/com/shima/patientchart/PatientchartApplication.java
@@ -0,0 +1,14 @@
+package com.shima.patientchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientchartApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PatientchartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shima/patientchart/PatientchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shima.patientchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
